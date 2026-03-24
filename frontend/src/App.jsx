316f6371af66b389c9437ebe54579c8389d09af0
Idealy Ryan <idealryan34@gmail.com>
2026-03-24 14:59:36 +0300

    the frontend for the 1.0 version

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="app-main">
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
